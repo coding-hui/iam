6d1a02e8e67f0fadd6b51da1440c715f1bfb40cc
YuHui-Liu <1515418211@qq.com>
2022-10-08 18:20:13 +0800

    update redis config

--- server/src/main/java/top/wecoding/iam/server/config/IAMServiceConfiguration.java
+++ server/src/main/java/top/wecoding/iam/server/config/IAMServiceConfiguration.java
@@ -1,8 +1,14 @@
 package top.wecoding.iam.server.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import top.wecoding.iam.server.props.AppProperties;
+import top.wecoding.redis.util.RedisUtils;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author liuyuhui
@@ -10,6 +16,15 @@ import top.wecoding.iam.server.props.AppProperties;
  * @qq 1515418211
  */
 @Configuration
+@RequiredArgsConstructor
 @EnableConfigurationProperties(AppProperties.class)
 public class IAMServiceConfiguration {
+
+  private final RedisTemplate<String, Object> redisTemplate;
+
+  @PostConstruct
+  public void init() {
+    redisTemplate.setValueSerializer(RedisSerializer.java());
+    RedisUtils.initialize(redisTemplate);
+  }
 }
