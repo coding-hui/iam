de2e5e705bcab488a622f83ec00cecd5f11cd53e
YuHui-Liu <1515418211@qq.com>
2022-10-08 15:52:06 +0800

    add wecoding-feign

--- server/src/main/java/top/wecoding/iam/server/WeCodingIamApplication.java
+++ server/src/main/java/top/wecoding/iam/server/WeCodingIamApplication.java
@@ -2,12 +2,12 @@ package top.wecoding.iam.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import top.wecoding.feign.annotation.EnableWeCodingFeignClients;
 import top.wecoding.iam.sdk.EnableIAMResourceServer;
 
-@EnableFeignClients(basePackages = "top.wecoding")
 @SpringBootApplication
 @EnableIAMResourceServer
+@EnableWeCodingFeignClients
 public class WeCodingIamApplication {
 
   public static void main(String[] args) {
