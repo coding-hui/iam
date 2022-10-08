5ceb68e695422112da22a80eb4e6cabda4848294
Liuyuhui <1515418211@qq.com>
2022-10-08 22:33:17 +0800

    update cache userdetails

--- server/src/main/java/top/wecoding/iam/server/service/impl/TokenServiceImpl.java
+++ server/src/main/java/top/wecoding/iam/server/service/impl/TokenServiceImpl.java
@@ -1,11 +1,6 @@
 package top.wecoding.iam.server.service.impl;
 
 import cn.hutool.core.util.StrUtil;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
@@ -15,11 +10,19 @@ import org.springframework.security.oauth2.server.authorization.OAuth2Authorizat
 import org.springframework.stereotype.Service;
 import top.wecoding.core.cache.CacheKey;
 import top.wecoding.core.result.PageInfo;
+import top.wecoding.iam.common.cache.UserDetailsCacheKeyBuilder;
 import top.wecoding.iam.common.cache.UserTokenCacheKeyBuilder;
 import top.wecoding.iam.common.constant.RedisConstant;
 import top.wecoding.iam.common.model.request.TokenInfoPageRequest;
 import top.wecoding.iam.common.model.response.TokenInfoResponse;
 import top.wecoding.iam.server.service.TokenService;
+import top.wecoding.redis.util.RedisUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author liuyuhui
@@ -46,7 +49,8 @@ public class TokenServiceImpl implements TokenService {
     if (accessToken == null || StrUtil.isBlank(accessToken.getToken().getTokenValue())) {
       return true;
     }
-    // TODO 清空用户信息
+    // 清空用户信息
+    RedisUtils.del(new UserDetailsCacheKeyBuilder().key(authorization.getPrincipalName()));
     // 清空access token
     authorizationService.remove(authorization);
 
