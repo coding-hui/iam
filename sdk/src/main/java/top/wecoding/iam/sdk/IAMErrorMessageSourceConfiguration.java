bfc6bcd27d63a81e9abf848ee12f25dd320e5f71
YuHui-Liu <1515418211@qq.com>
2022-10-09 16:01:35 +0800

    reformat & update helm resource

--- sdk/src/main/java/top/wecoding/iam/sdk/IAMErrorMessageSourceConfiguration.java
+++ sdk/src/main/java/top/wecoding/iam/sdk/IAMErrorMessageSourceConfiguration.java
@@ -1,14 +1,13 @@
 package top.wecoding.iam.sdk;
 
+import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.SERVLET;
+
+import java.util.Locale;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Locale;
-
-import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.SERVLET;
 
 /**
  * @author liuyuhui
--- server/src/main/java/top/wecoding/iam/server/authentication/base/OAuth2ResourceOwnerBaseAuthenticationProvider.java
+++ server/src/main/java/top/wecoding/iam/server/authentication/base/OAuth2ResourceOwnerBaseAuthenticationProvider.java
@@ -73,8 +73,7 @@ public abstract class OAuth2ResourceOwnerBaseAuthenticationProvider<
     this.tokenGenerator = tokenGenerator;
 
     // 国际化配置
-    this.messages =
-        new MessageSourceAccessor(SpringUtil.getBean("iamMessageSource"), Locale.CHINA);
+    this.messages = new MessageSourceAccessor(SpringUtil.getBean("iamMessageSource"), Locale.CHINA);
   }
 
   @Deprecated
