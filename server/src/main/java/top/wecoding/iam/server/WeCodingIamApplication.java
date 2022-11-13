732045bde478e30d7d16d82c8e90756493ceb09e
Liuyuhui <1515418211@qq.com>
2022-11-13 17:12:49 +0800

    remote demo config

--- server/src/main/java/top/wecoding/iam/server/WeCodingIamApplication.java
+++ server/src/main/java/top/wecoding/iam/server/WeCodingIamApplication.java
@@ -1,6 +1,5 @@
 package top.wecoding.iam.server;
 
-import java.util.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import top.wecoding.feign.annotation.EnableWeCodingFeignClients;
@@ -12,8 +11,6 @@ import top.wecoding.iam.sdk.EnableIAMResourceServer;
 public class WeCodingIamApplication {
 
   public static void main(String[] args) {
-    Properties properties = System.getProperties();
-    properties.setProperty("spring.cloud.nacos.config.server-addr", "http://wecoding.top:8848");
     SpringApplication.run(WeCodingIamApplication.class, args);
   }
 }
