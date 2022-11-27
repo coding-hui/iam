31f000ca795a7ba9ae7f1ef220e6cbf11103d9c7
liuyuhui <wecoding@yeah.net>
2022-11-28 01:14:26 +0800

    add mysql connector & upgrade tools to 0.5.1

--- iam-server/src/main/java/top/wecoding/iam/server/WeCodingIamApplication.java
+++ iam-server/src/main/java/top/wecoding/iam/server/WeCodingIamApplication.java
@@ -4,15 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Properties;
-
 @Slf4j
 @SpringBootApplication(scanBasePackages = "top.wecoding.iam")
 public class WeCodingIamApplication {
 
   public static void main(String[] args) {
-    Properties properties = System.getProperties();
-    properties.setProperty("spring.cloud.nacos.config.server-addr", "http://localhost:8848");
     SpringApplication.run(WeCodingIamApplication.class, args);
     log.info("{} started successfully.", WeCodingIamApplication.class.getSimpleName());
   }
