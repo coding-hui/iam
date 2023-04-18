f72a20fd2070f4ae0377849aaf81b7dab01043f1
coding-hui <wecoding@yeah.net>
2023-04-18 20:11:08 +0800

    add service ut

---
+++ internal/apiserver/domain/service/service_test.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 coding-hui. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package service
+
+import (
+	"gotest.tools/assert"
+	"testing"
+
+	"github.com/coding-hui/iam/internal/apiserver/config"
+)
+
+func TestInitServiceBean(t *testing.T) {
+	assert.Equal(t, len(InitServiceBean(config.Config{})), 2)
+}
