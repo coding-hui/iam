66d56f65a0e12ac2c9b651333b3a86525162c456
wecoding <wecoding@yeah.net>
2025-10-19 20:35:25 +0800

    refactor: consolidate API route registration
    - Remove separate router registration method and consolidate routes
    - Update API group registration to use version prefix and authentication middleware
    - Remove duplicate route definitions for API key endpoints
    
    Signed-off-by: wecoding <wecoding@yeah.net>

--- internal/apiserver/interfaces/api/apikey.go
+++ internal/apiserver/interfaces/api/apikey.go
@@ -26,25 +26,9 @@ func NewApiKey() Interface {
 	return &apiKey{}
 }
 
-// RegisterRouter register apiKey router to gin router group.
-func (a *apiKey) RegisterRouter(g *gin.RouterGroup) {
-	apiv1 := g.Group(versionPrefix+"/apikeys").Use(autoAuthCheck.AuthFunc(), permissionCheckFunc("apikeys"))
-	{
-		apiv1.POST("", a.createApiKey)
-		apiv1.PUT("/:instanceId", a.updateApiKey)
-		apiv1.DELETE("/:instanceId", a.deleteApiKey)
-		apiv1.GET("/:instanceId", a.getApiKey)
-		apiv1.GET("", a.listApiKeys)
-
-		apiv1.PUT("/:instanceId/enable", a.enableApiKey)
-		apiv1.PUT("/:instanceId/disable", a.disableApiKey)
-		apiv1.POST("/:instanceId/regenerate", a.regenerateSecret)
-	}
-}
-
 // RegisterApiGroup register apiKey router to gin engine.
 func (a *apiKey) RegisterApiGroup(g *gin.Engine) {
-	apiv1 := g.Group("/v1/apikeys")
+	apiv1 := g.Group(versionPrefix+"/apikeys").Use(autoAuthCheck.AuthFunc(), permissionCheckFunc("apikeys"))
 	{
 		apiv1.POST("", a.createApiKey)
 		apiv1.PUT("/:instanceId", a.updateApiKey)
