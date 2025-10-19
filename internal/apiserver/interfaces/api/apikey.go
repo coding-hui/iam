338d2c42a7e17e79fb410000361c840a0a125d08
wecoding <wecoding@yeah.net>
2025-10-19 20:28:42 +0800

    Revert "feat: secure API key authentication"
    This reverts commit ba61611edcc6cdbe7355f06ceb0d5d02ee227a73.

--- internal/apiserver/interfaces/api/apikey.go
+++ internal/apiserver/interfaces/api/apikey.go
@@ -28,7 +28,7 @@ func NewApiKey() Interface {
 
 // RegisterRouter register apiKey router to gin router group.
 func (a *apiKey) RegisterRouter(g *gin.RouterGroup) {
-	apiv1 := g.Group(versionPrefix+"/apikeys").Use(autoAuthCheck.AuthFunc(), permissionCheckFunc("apikeys"))
+	apiv1 := g.Group("/v1/apikeys")
 	{
 		apiv1.POST("", a.createApiKey)
 		apiv1.PUT("/:instanceId", a.updateApiKey)
